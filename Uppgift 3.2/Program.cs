440e5d40907b35af30b750bfc2dd542eb11cab89
losc06001 <losc06001@E-PW0234JE>
2023-10-04 10:33:17 +0200

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Uppgift3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? Svara med J för ja och N för nej");
+            string gymnasiet = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            string gammal = Console.ReadLine();
+            int ålder = int.Parse(gammal);
+            if (ålder <= 21 && gymnasiet == "j") 
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            }
+
+        }
+    } }
